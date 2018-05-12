0d9e31c9035cf1a1f7eb978f22130f8e7eb24403
nithin <nithin19484@gmail.com>
2018-05-12 22:42:16 +0530

    Adding examples

---
+++ examples/example1.py
@@ -0,0 +1,44 @@
+import pyrg as rg
+import numpy as np
+from scipy.stats import multivariate_normal
+
+
+def make_gaussian(size=(30,30,30),mu=(0,0,0),sigma=(0.25,0.25,0.25)):
+
+    x, y, z = np.mgrid[-1.0:1.0:30j, -1.0:1.0:30j,-1.0:1.0:30j]
+    # Need an (N, 2) array of (x, y) pairs.
+    xyz = np.column_stack([x.flat, y.flat,z.flat])
+
+    mu = np.array(mu)
+
+    sigma = np.array(sigma)
+    covariance = np.diag(sigma**2)
+
+    f = multivariate_normal.pdf(xyz, mean=mu, cov=covariance)
+
+    # Reshape back to a (30, 30) grid.
+    f = f.reshape(x.shape)
+    
+    f = np.array(f,dtype=np.float32)
+    
+    return f
+
+
+def make_function():
+    #make a gaussian with 3 features
+    arr   = make_gaussian(mu=(0,0,0))  
+    arr  += make_gaussian(mu=(0.5,0,0))  
+    arr  += make_gaussian(mu=(0.5,-0.5,0.5))
+    return arr
+
+    
+
+if __name__=="__main__":
+    make_function()
+    
+    g  = rg.Grid3D((30,30,30))
+    a  = np.array(g, copy = False)    
+    a[:] = make_function()
+    
+    mt =  rg.MergeTree()
+    mt.computeContourTree(g)
--- pymain.cpp
+++ pymain.cpp
@@ -6,6 +6,7 @@
 
 #include "utl.h"
 #include "Grid3D.hpp"
+#include "MergeTree.hpp"
 
 using namespace contourtree;
 
@@ -24,16 +25,25 @@ PYBIND11_MODULE(pyrg, m) {
                 return new Grid3D(shape[2].cast<int>(),shape[1].cast<int>(),shape[0].cast<int>());}))
             .def("atXYZ",[](Grid3D &m, int x,int y, int z){return m.getFunctionValue(m.index(x,y,z));})
             .def_buffer([](Grid3D &m) -> py::buffer_info {
-            return py::buffer_info(
-                m.data(),                                  /* Pointer to buffer */
-                sizeof(scalar_t),                          /* Size of one scalar */
-                py::format_descriptor<scalar_t>::format(), /* Python struct-style format descriptor */
-                3,                                         /* Number of dimensions */
-                { m.dimZ(), m.dimY(),m.dimX()},            /* Buffer dimensions */
-                { sizeof(scalar_t) * m.dimX()*m.dimY(),    /* Strides (in bytes) for each index */
-                  sizeof(scalar_t) * m.dimX(),
-                  sizeof(scalar_t) }
-            );
-        });
+                return py::buffer_info(
+                    m.data(),                                  /* Pointer to buffer */
+                    sizeof(scalar_t),                          /* Size of one scalar */
+                    py::format_descriptor<scalar_t>::format(), /* Python struct-style format descriptor */
+                    3,                                         /* Number of dimensions */
+                    { m.dimZ(), m.dimY(),m.dimX()},            /* Buffer dimensions */
+                    { sizeof(scalar_t) * m.dimX()*m.dimY(),    /* Strides (in bytes) for each index */
+                      sizeof(scalar_t) * m.dimX(),
+                      sizeof(scalar_t) });
+            })
+    ;
+
+
+    py::class_<MergeTree>(m, "MergeTree", py::buffer_protocol())
+            .def(py::init<>())
+            .def("computeContourTree",[](MergeTree& ct,Grid3D& grid)->void{ct.computeTree(&grid,TypeContourTree);})
+            .def("computeJoinTree",[](MergeTree& ct,Grid3D& grid)->void{ct.computeTree(&grid,TypeJoinTree);})
+            .def("computeSplitTree",[](MergeTree& ct,Grid3D& grid)->void{ct.computeTree(&grid,TypeSplitTree);})
+    ;
+
 
 }
